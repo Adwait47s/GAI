027ebf11f9f91fe0a2753804968e61dd8a2b0671
Sanika Inamdar <86364275+SanikaInamdar@users.noreply.github.com>
2023-10-10 23:18:06 +0530

    User apis and file upload(incomplete), login api

--- src/UserDashboard.js
+++ src/UserDashboard.js
@@ -1,21 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { storage } from './firebase';
+import { ref, uploadBytes } from 'firebase/storage';
+import {v4} from 'uuid';
 
 function UserPage() {
   const [selectedPdf, setSelectedPdf] = useState(null);
-  const [pdfSummary, setPdfSummary] = useState(''); 
+  const [pdfSummary, setPdfSummary] = useState('');
+  const [documents, setDocuments] = useState([]);
+  const [jwtToken] = useState(localStorage.getItem('jwtToken'));
 
-  let file
-  
+  const fetchDocuments = async () => {
+    try {
+      const response = await fetch('https://word-extractor-apis.onrender.com/files', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setDocuments(data.files);
+      }
+    } catch (error) {
+      console.error('Error fetching documents:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDocuments();
+  }, []); // Fetch documents when the component mounts
+
   const handlePdfDrop = (event) => {
     event.preventDefault();
-    file = event.dataTransfer.files[0];
+    const file = event.dataTransfer.files[0];
     if (file && file.type === 'application/pdf') {
       setSelectedPdf(file);
     }
   };
 
   const handlePdfSelect = (event) => {
-    file = event.target.files[0];
+    const file = event.target.files[0];
     if (file && file.type === 'application/pdf') {
       setSelectedPdf(file);
     }
@@ -23,26 +48,54 @@ function UserPage() {
 
   const clearSelectedPdf = () => {
     setSelectedPdf(null);
-    setPdfSummary(''); 
+    setPdfSummary('');
   };
 
-  const handleUpload = () => {
+  const handleUpload = async () => {
     if (!selectedPdf) {
       alert('No PDF chosen. Please select a PDF file before uploading.');
       return;
     }
-    // backend, file should be on backend
-    const dummySummary = 'This is a dummy PDF summary.';
+    const storageRef = ref(storage, `${selectedPdf.name + v4()}`);
+    uploadBytes(storageRef, selectedPdf).then(( ) => {
+      alert('Uploaded a file!');
+    });
 
-    // backend, replace the summary with the backend summary
-    setPdfSummary(dummySummary);
+    
 
-    setSelectedPdf(null);
+    const formData = new FormData();
+    formData.append('file_name', selectedPdf.name);
+    formData.append('file_content', null);
+    formData.append('file_link', 'link');
+
+    try {
+      const response = await fetch('https://word-extractor-apis.onrender.com/files', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+        body: formData,
+      });
+
+      if (response.ok) {
+        // Successful upload, fetch documents again to update the table
+        fetchDocuments();
+      } else {
+        // Handle upload error
+        const errorData = await response.json();
+        console.error('Upload error:', errorData.message || 'An error occurred during upload.');
+      }
+    } catch (error) {
+      console.error('Network error during upload:', error);
+    } finally {
+      clearSelectedPdf();
+    }
   };
 
+
   return (
-    <div className="bg-blue-200">
-      <h1 className="text-black text-center text-4xl font-bold  py-10">Welcome User !</h1>
+    <div className="bg-blue-200 h-screen p-8">
+      <h1 className="text-black text-center text-4xl font-bold  py-10">Welcome User!</h1>
       <div
         className="bg-slate-100 text-center mx-96 py-6 border-2 rounded-lg border-black space-y-5"
         onDragOver={(e) => e.preventDefault()}
@@ -84,10 +137,44 @@ function UserPage() {
         </button>
       </div>
 
-      <p className="font-bold mx-32 p-1 m-1">PDF Summary:</p>
+      {/* <p className="font-bold mx-32 p-1 m-1">PDF Summary:</p>
       <div className="border-2 border-black mx-32 pb-80 rounded-md pl-2 m-1">
         {pdfSummary}
-      </div>
+      </div> */}
+      {documents === null ? (
+        <p className="text-center text-2xl mt-4">Loading...</p>
+      ) : documents.length === 0 ? (
+        <p className="text-center text-2xl mt-4">No documents available.</p>
+      ):(
+        <table className="w-full border-collapse border border-blue-500 mt-4">
+          <thead>
+            <tr>
+              <th className="border border-blue-500 p-2">Document Name</th>
+              <th className="border border-blue-500 p-2">Updated Date (IST)</th>
+              <th className="border border-blue-500 p-2">Content</th>
+              <th className="border border-blue-500 p-2">Download Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {documents.map((document) => (
+              <tr key={document.id}>
+                <td className="border border-blue-500 p-2">{document.name}</td>
+                <td className="border border-blue-500 p-2">
+                  {new Date(document.updated_at).toLocaleString('en-US', {
+                    timeZone: 'Asia/Kolkata',
+                  })}
+                </td>
+                <td className="border border-blue-500 p-2">{JSON.stringify(document.content)}</td>
+                <td className="border border-blue-500 p-2">
+                  <a href={document.link} download>
+                    Download
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
